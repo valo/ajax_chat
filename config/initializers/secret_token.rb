097fa3562a5cab6273c09a32a81fddbed7196a68
Valentin Mihov <valentin.mihov@gmail.com>
2011-01-31 02:14:22 +0200

    The skeleton of the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxChat::Application.config.secret_token = '7ab1da54d57f3cd3d1cd78c3f2811657740834a06715b0e8d9874f3c5dd72d7c7468d66d15437b359f3af3d21fd7c540f644cf094620e29d4381128e3f6c23a3'
